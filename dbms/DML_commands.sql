38ffe0952b4b70c5e4585008d24507bd35664077
ferrary7 <ary7sharma@gmail.com>
2023-10-30 10:47:01 +0530

    Minor Changes

--- dbms/DML_commands.sql
+++ dbms/DML_commands.sql
@@ -1,36 +1,36 @@
-INSERT INTO User (Email, FName, SName, Hashed_Password)
-VALUES ('ary7sharma@gmail.com', 'Ary', 'Sharma', 'hashed_password');
+INSERT INTO Users (Email, FName, SName, Hashed_Password)
+VALUES ('aryan.sharma@kalvium.community', 'Aryan', 'Sharma', 'password');
 
-UPDATE User 
-SET FName = 'Aryan', SName = 'Sharma' 
-WHERE Email = 'ary7sharma@gmail.com';
+UPDATE Users
+SET FName = 'Ary', SName = 'Diesel' 
+WHERE Email = 'aryan.sharma@kalvium.community';
 
-SELECT * FROM User;
+SELECT * FROM Users;
 
 INSERT INTO IncomeCategory (Inc_Cat_ID, Inc_Cat)
-VALUES (1,  'Salary')
+VALUES (2,  'Salary')
 
 INSERT INTO ExpenseCategory (Exp_Cat_ID, Exp_Cat)
-VALUES (1, 'Rent');
+VALUES (2, 'Rent');
 
 INSERT INTO InvestmentCompany (Inv_Symbol, Comp_Name)
-VALUES ('AAPL', 'Apple Inc.');
+VALUES ('GOOGL', 'Google');
 
 INSERT INTO InvestmentType (Inv_Type_ID, Inv_Type)
-VALUES (1, 'Stocks');
+VALUES (2, 'Stocks');
 
 INSERT INTO Transaction (Transaction_ID, Email, Transaction_Date, Transaction_Details, Transaction_Amount, Transaction_Type, Income_Category_ID)
-VALUES (1, 'ary7sharma@gmail.com', '2023-09-23', 'Monthly Salary', 25000.00, 'Income', 1);
+VALUES (5, 'aryan.sharma@kalvium.community', '2023-10-19', 'Protein Powder', 8000.00, 'Expense', 1);
 
 DELETE FROM Transaction 
-WHERE Transaction_ID = 1;
+WHERE Transaction_ID = 5;
 
 SELECT * FROM Transaction 
-WHERE Email = 'ary7sharma@gmail.com';
+WHERE Email = 'aryan.sharma@kalvium.community';
 
 INSERT INTO Investment (Investment_ID, Email, Date_of_Investment, Inv_Symbol, Investment_Amount, Investment_Type_ID)
-VALUES (1, 'ary7sharma@gmail.com', '2023-09-24', 'AAPL', 3000.00, 1);
+VALUES (2, 'aryan.sharma@kalvium.community', '2023-10-19', 'GOOGL', 8000.00, 1);
 
 INSERT INTO SavingsGoal (ID, UserID, Goal_Amount, Current_Amount, Target_Date)
-VALUES (1, 'ary7sharma@gmail.com', 10000.00, 2000.00, '2023-12-31');
+VALUES (2, 'aryan.sharma@kalvium.community', 20000.00, 3000.00, '2023-12-31');
 
